583328ec35fc469d52a6c0f7f7d00e55144768d1
yauheni_karpovich <yauheni_karpovich@bitnet.by>
2021-12-19 21:25:47 +0100

    fix

--- src/modules/main/Contacts/index.js
+++ src/modules/main/Contacts/index.js
@@ -8,11 +8,11 @@ const Form = () => {
             <S.Content>kropka.service@gmail.com</S.Content>
             <S.SubTitle>Phone</S.SubTitle>
             <S.Content>+48 516123134</S.Content>
-            <S.LinksWrapper>
-                <S.Link href={"/hui"} color="#3B5998" imgUrl="/Facebook.png"/>
-                <S.Link href={"/hui"} color="#0077B5" imgUrl="/linkedIn.png"/>
-                <S.Link href={"/hui"} color="#37AEE2" imgUrl="/telegram.png"/>
-            </S.LinksWrapper>
+            {/*<S.LinksWrapper>*/}
+            {/*    <S.Link href={"/hui"} color="#3B5998" imgUrl="/Facebook.png"/>*/}
+            {/*    <S.Link href={"/hui"} color="#0077B5" imgUrl="/linkedIn.png"/>*/}
+            {/*    <S.Link href={"/hui"} color="#37AEE2" imgUrl="/telegram.png"/>*/}
+            {/*</S.LinksWrapper>*/}
         </S.Wrapper>
     );
 }
